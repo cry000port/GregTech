da43baa7611b3327cd7be83315b99c7a68f8e9af
cry000port <33923657+cry000port@users.noreply.github.com>
2017-11-23 16:07:21 +0700

    Update CraftRecipe.js
    Изменены id

--- GregTech/dev/Recipes/CraftRecipes.js
+++ GregTech/dev/Recipes/CraftRecipes.js
@@ -5,18 +5,18 @@ Recipes.addShaped({id: ItemID.EmptyShapePlate, count: 1, data: 0}, ["   "," a ",
 Recipes.addShaped({id: ItemID.EmptyShapePlate, count: 1, data: 0}, ["   "," a ","   "], ['a', ItemID.MoldIngot, 0]);
 Recipes.addShaped({id: ItemID.EmptyShapePlate, count: 1, data: 0}, ["   "," a ","   "], ['a', ItemID.MoldNuggets, 0]);
 Recipes.addShaped({id: ItemID.EmptyShapePlate, count: 1, data: 0}, ["   "," a ","   "], ['a', ItemID.MoldBlock, 0]);
-var recMortar=[[ItemID.GoldMortar, 266],[ItemID.BronzeMortar, ItemID.BronzeIngot],[ItemID.DiamondMortar, 264],[ItemID.FlintMortar, 318],[ItemID.LeadMortar, ItemID.LeadIngot],[ItemID.SilverMortar, ItemID.SilverIngot],[ItemID.SteelMortar, ItemID.SteelIngot],[ItemID.IronMortar, 265]];
+var recMortar=[[ItemID.mortarGold, 266],[ItemID.mortarBronze, ItemID.ingotBronze],[ItemID.mortarDiamond, 264],[ItemID.mortarFlint, 318],[ItemID.mortarLead, ItemID.ingotLead],[ItemID.mortarSilver, ItemID.ingotSilver],[ItemID.mortarSteel, ItemID.ingotSteel],[ItemID.mortarIron, 265]];
 for(var i in recMortar){
 Recipes.addShaped({id: recMortar[i][0], count: 1, data: 0}, [" a ","bab","bbb"], ['b', 1, 0, 'a', recMortar[i][1], 0]);
 }
 
-var tinydust=[[ItemID.TinDust,ItemID.TinyPileTinDust],[ItemID.IronDust, ItemID.TinyPileIronDust],[ItemID.GoldDust, ItemID.TinyPileGoldDust],
-[ItemID.CopperDust, ItemID.TinyPileCopperDust],[ItemID.LeadDust, ItemID.TinyPileLeadDust],[ItemID.SilverDust, ItemID.TinyPileSilverDust],[ItemID.RubberDust, ItemID.TinyPileRubberDust],[ItemID.BronzeDust, ItemID.TinyPileBronzeDust],[ItemID.AntimonyDust, ItemID.TinyPileAntimonyDust],
-[ItemID.DiamondDust, ItemID.TinyPileDiamondDust],[ItemID.EmeraldDust, ItemID.TinyPileEmeraldDust],[ItemID.SteelDust, ItemID.TinyPileSteelDust],[ItemID.AshesDust, ItemID.TinyPileAshesDust],[ItemID.DarkAshesDust, ItemID.TinyPileDarkAshesDust]];
+var tinydust=[[ItemID.dustTin,ItemID.tinydustTin],[ItemID.dustIron, ItemID.tinydustIron],[ItemID.dustGold, ItemID.tinydustGold],
+[ItemID.dustCopper, ItemID.tinydustCopper],[ItemID.dustLead, ItemID.tinydustLead],[ItemID.dustSilver, ItemID.tinydustSilver],[ItemID.dustRubber, ItemID.tinydustRubber],[ItemID.dustBronze, ItemID.tinydustBronze],[ItemID.dustAntimony, ItemID.tinydustAntimony],
+[ItemID.dustDiamond, ItemID.tinydustDiamond],[ItemID.dustEmerald, ItemID.tinydustEmerald],[ItemID.dustSteel, ItemID.tinydustSteel],[ItemID.dustAshes, ItemID.tinydustAshes],[ItemID.dustDarkAshes, ItemID.tinydustDarkAshes]];
 
-var smalldust=[[ItemID.TinDust,ItemID.SmallPileTinDust],[ItemID.IronDust, ItemID.SmallPileIronDust],[ItemID.GoldDust, ItemID.SmallPileGoldDust],
-[ItemID.CopperDust, ItemID.SmallPileCopperDust],[ItemID.LeadDust, ItemID.SmallPileLeadDust],[ItemID.SilverDust, ItemID.SmallPileSilverDust],[ItemID.RubberDust, ItemID.SmallPileRubberDust],[ItemID.BronzeDust, ItemID.SmallPileBronzeDust],[ItemID.AntimonyDust, ItemID.SmallPileAntimonyDust],
-[ItemID.DiamondDust, ItemID.SmallPileDiamondDust],[ItemID.EmeraldDust, ItemID.SmallPileEmeraldDust],[ItemID.SteelDust, ItemID.SmallPileSteelDust]];
+var smalldust=[[ItemID.dustTin,ItemID.smalldustTin],[ItemID.dustIron, ItemID.smalldustIron],[ItemID.dustGold, ItemID.smalldustGold],
+[ItemID.dustCopper, ItemID.smalldustCopper],[ItemID.dustLead, ItemID.smalldustLead],[ItemID.dustSilver, ItemID.smalldustSilver],[ItemID.dustRubber, ItemID.smalldustRubber],[ItemID.dustBronze, ItemID.smalldustBronze],[ItemID.dustAntimony, ItemID.smalldustAntimony],
+[ItemID.dustDiamond, ItemID.smalldustDiamond],[ItemID.dustEmerald, ItemID.smalldustEmerald],[ItemID.dustSteel, ItemID.smalldustSteel]];
 for(var i in tinydust){
 Recipes.addShaped({id: tinydust[i][0], count: 1, data: 0}, ["bbb","bbb","bbb"], ['b', tinydust[i][1], 0]);
 Recipes.addShaped({id: tinydust[i][1], count: 9, data: 0}, ["  "," b"], ['b', tinydust[i][0], 0]);
@@ -25,4 +25,4 @@ for(var i in smalldust){
 Recipes.addShaped({id: smalldust[i][0], count: 1, data: 0}, ["bb","bb"], ['b', smalldust[i][1], 0]);
 Recipes.addShaped({id: smalldust[i][1], count: 4, data: 0}, ["  "," b"], ['b', smalldust[i][0], 0]);
 }
-Recipes.addShaped({id: ItemID.BronzeDust, count: 3, data: 0}, ["aa","ab"], ['b', ItemID.TinDust, 0, 'a', ItemID.CopperDust, 0]);
+Recipes.addShaped({id: ItemID.dustBronze, count: 3, data: 0}, ["aa","ab"], ['b', ItemID.dustTin, 0, 'a', ItemID.dustCopper, 0]);
